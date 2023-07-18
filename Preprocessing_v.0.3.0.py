69a7f83fadacf1b1340bcdbdf128dd7bece16f20
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2023-07-18 20:04:46 +0900

    Add files via upload
    Preprocessing_v.0.3.0에서 돌려보시면 됩니다!

---
+++ Preprocessing_v.0.3.0.py
@@ -0,0 +1,20 @@
+import GPS_preprocessing_v_0_3_0
+
+# import POI_clustering
+# import Transition_Matrix
+
+
+if __name__ == "__main__":
+    
+    # GPS_preprocessing.GPS_data_preprocessing()
+    
+    # POI_clustering.make_POI_csv()
+    
+    # for month in range(1, 13):
+    #     GPS_preprocessing_v_0_3_0.trajectory_csv(month)
+    #     Transition_Matrix.make_transition_matrix_csv(month)
+    
+    
+    GPS_preprocessing_v_0_3_0.data_preprocessing("봄")
+    
+    pass
